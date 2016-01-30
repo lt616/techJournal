8f98194f949d26332e4b6673d076c29fc9b1b1bc
lt616 <langyayue616@sina.com>
2016-01-30 18:45:43 +1100

    20160130

--- readFile.php
+++ readFile.php
@@ -1,8 +1,8 @@
 <?php
 	$fileName = $_POST['FNAME'];
-	$myfile = fopen($fileName, "r") or die("Unable to open the file" + $fileName);
 	echo $fileName;
-	echo fread($myfile, filesize($fileName));
+	//$myfile = fopen($fileName, "r") or die("Unable to open the file" + $fileName);
+	//echo fread($myfile, filesize($fileName));
 
 	fclose($myfile);			
 ?>
