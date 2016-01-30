b269a28aba23a5235426e585df38a87d5d263e9e
lt616 <langyayue616@sina.com>
2016-01-30 18:47:27 +1100

    20160130

--- readFile.php
+++ readFile.php
@@ -1,8 +1,7 @@
 <?php
 	$fileName = $_GET['FNAME'];
-	echo $fileName;
-	//$myfile = fopen($fileName, "r") or die("Unable to open the file" + $fileName);
-	//echo fread($myfile, filesize($fileName));
+	$myfile = fopen($fileName, "r") or die("Unable to open the file" + $fileName);
+	echo fread($myfile, filesize($fileName));
 
 	fclose($myfile);			
 ?>
